97a5f762cd1585a63b419028cb25fb74a972960d
Erica Mae L. Fernandez <fernandezericamaez@gmail.com>
2026-05-22 21:44:50 +0800

    Update AuthWindowFactory.java

--- src/main/java/fernandez/student_management_system/factory/AuthWindowFactory.java
+++ src/main/java/fernandez/student_management_system/factory/AuthWindowFactory.java
@@ -1,21 +1,41 @@
-package com.example.test_student_management.factory;
+package fernandez.student_management_system.factory;
 
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 
 import java.io.IOException;
+import java.net.URL;
 
 public final class AuthWindowFactory {
+
     private AuthWindowFactory() {
     }
 
     public static Scene createLoginScene() throws IOException {
-        FXMLLoader loader = new FXMLLoader(AuthWindowFactory.class.getResource("/com/example/test_student_management/login-view.fxml"));
+
+        URL fxml = AuthWindowFactory.class.getResource(
+                "/fernandez/student_management_system/login-view.fxml"
+        );
+
+        if (fxml == null) {
+            throw new IOException("login-view.fxml NOT FOUND. Check resources folder path.");
+        }
+
+        FXMLLoader loader = new FXMLLoader(fxml);
         return new Scene(loader.load(), 420, 300);
     }
 
     public static Scene createSignUpScene() throws IOException {
-        FXMLLoader loader = new FXMLLoader(AuthWindowFactory.class.getResource("/com/example/test_student_management/signup-view.fxml"));
+
+        URL fxml = AuthWindowFactory.class.getResource(
+                "/fernandez/student_management_system/signup-view.fxml"
+        );
+
+        if (fxml == null) {
+            throw new IOException("signup-view.fxml NOT FOUND. Check resources folder path.");
+        }
+
+        FXMLLoader loader = new FXMLLoader(fxml);
         return new Scene(loader.load(), 420, 360);
     }
 }
