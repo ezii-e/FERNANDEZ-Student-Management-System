be9967813b07703c38eedb7cfe3eaff6b2d3f612
Erica Mae L. Fernandez <fernandezericamaez@gmail.com>
2026-05-22 21:45:30 +0800

    Update StudentWindowFactory.java

--- src/main/java/fernandez/student_management_system/factory/StudentWindowFactory.java
+++ src/main/java/fernandez/student_management_system/factory/StudentWindowFactory.java
@@ -1,16 +1,27 @@
-package com.example.test_student_management.factory;
+package fernandez.student_management_system.factory;
 
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 
 import java.io.IOException;
+import java.net.URL;
 
 public final class StudentWindowFactory {
+
     private StudentWindowFactory() {
     }
 
     public static Scene createScene() throws IOException {
-        FXMLLoader loader = new FXMLLoader(StudentWindowFactory.class.getResource("/com/example/test_student_management/student-view.fxml"));
+
+        URL fxml = StudentWindowFactory.class.getResource(
+                "/fernandez/student_management_system/student-view.fxml"
+        );
+
+        if (fxml == null) {
+            throw new IOException("student-view.fxml NOT FOUND. Check resources folder path.");
+        }
+
+        FXMLLoader loader = new FXMLLoader(fxml);
         return new Scene(loader.load());
     }
 }
